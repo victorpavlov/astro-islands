cdc97e9cce68bb34d6b0d3683b332b91c7fca3a6
Victor Pavlov <vitekpavlov@gmail.com>
2024-02-20 17:18:35 +0200

    Add sample island component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), preact()]
 });
